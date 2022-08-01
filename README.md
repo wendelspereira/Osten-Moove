eff234f9d49904f0d8da90762056c8de1ee2254f
Wendel S. Pereira <wendelspereira@outlook.com>
2022-08-01 02:12:32 -0300

    _

---
+++ README.md
@@ -0,0 +1,7 @@
+# Osten-Moove
+
+## Iniciando
+
+Baixe o repositório e siga os procedimentos presentes nos arquivos READMEs da pasta 'font-end' e da pasta 'back-end' para configurar e executar o projeto.
+
+Para acessar uma cópia em produção desta aplicação, [clique aqui](https://test-osten-move-front-end.vercel.app/).
--- back-end/src/shared/infra/http/routes/index.ts
+++ back-end/src/shared/infra/http/routes/index.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { businessRoutes } from "./business.routes";
 
 const routes = express();
+
+routes.use('/', (req: Request, res: Response) => {
+    return res.json({welcome: "Osten Moove API"})
+})
 
 routes.use('/business', businessRoutes);
 
